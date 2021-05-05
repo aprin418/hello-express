6ded485ae8be6568142ab4c7d06eed98d9865df5
aprin418 <aprin418@gmail.com>
2021-05-05 11:09:26 -0400

    Express codealong

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // imports
 const express = require("express");
+const axios = require("axios");
+const { response } = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
@@ -10,10 +12,48 @@ app.get("/", function (req, res) {
   res.send("Hello World!");
 });
 
-app.get("/sei", (req, res) => {
-  res.send("SEI 412");
+// app.get("/sei", (req, res) => {
+//   res.send("SEI 412");
+// });
+
+app.get("/rockets", (req, res) => {
+  axios
+    .get("https://api.spacexdata.com/v3/rockets")
+    .then((response) => {
+      console.log(response.data);
+      res.json(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
+app.get("/greet/:name", (req, res) => {
+  console.log(req.params);
+  res.send(`Hello ${req.params.name}`);
+});
+
+app.get("/github/:username", (req, res) => {
+  const { username } = req.params;
+  axios
+    .get(`https://api.github.com/users/${username}`)
+    .then((response) => {
+      console.log(response.data);
+      res.json(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+//last route, takes you to 404
+//always have at the end of page
+app.get("/*", (req, res) => {
+  console.log(req.params);
+  res.send("404");
+});
+
+// listen is always last item on page
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
